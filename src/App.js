8e2983a6c04eda2a6ecbf24fd4b583d82182ceac
Nick <nick@Nicks-MacBook-Pro.local>
2019-08-05 16:21:14 +0100

    Making sure everything's still set up

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
             playingId !== null ? "play-button-playing" : ""
           }`}
         >
-          {playingId !== null ? "Pop you off" : "Pop you on"}
+          {playingId !== null ? "Pop you off!" : "Pop you on!"}
         </div>
 
         {sound && sound.duration() > 0 && (
